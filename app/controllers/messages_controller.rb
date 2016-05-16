8a6dea6977af266f1fce92614352e4195ef285c1
Freddy Rodriguez <SaulARodriguez@a2zmacbook.local>
2016-05-16 12:03:41 -0500

    Add User ID to Messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,20 @@ class MessagesController < ApplicationController
 	before_action :find_message, only: [:show, :edit, :update, :destroy]
 	
 	def index
-		@messages = Message.all.order('created_at DESC')
+		if user_signed_in?
+			@messages = current_user.messages.order('created_at DESC')
+		end
 	end
 	
 	def show
 	end
 	
 	def new
-		@message = Message.new
+		@message = current_user.messages.build
 	end
 	
 	def create
-		@message = Message.new(message_params)
+		@message = current_user.messages.build(message_params)
 		if @message.save
 			redirect_to root_path
 		else
