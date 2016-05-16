9dcc7e51f747800e0997aae69d4538ab598dcb0a
Freddy Rodriguez <SaulARodriguez@a2zmacbook.local>
2016-05-16 12:24:12 -0500

    Add Comments to Message View File

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 	def create
 		@message = Message.find(params[:message_id])
 		@comment = @message.comments.create(comment_params)
-		@comment.user_id = current_user.user_id
+		@comment.user_id = current_user.id
 		
 		if @comment.save
 			redirect_to message_path(@message)
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
 	end
 	
 	private
-		def comments_param
+		def comment_params
 			params.require(:comment).permit(:content)
 		end
 end
